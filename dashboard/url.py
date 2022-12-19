4c7548d887fdbdcfd753ae6cafbd82d9ac8a4be9
dev csis <dev@csis.or.id>
2022-12-19 15:59:48 +0700

    adding other pages

--- dashboard/url.py
+++ dashboard/url.py
@@ -3,9 +3,5 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='dashboard/index.html'), name='home'),
-    path('about/', TemplateView.as_view(template_name='web/index.html'), name='about'),
-    path('our-services/', TemplateView.as_view(template_name='web/index.html'), name='services'),
-    path('contact/', TemplateView.as_view(template_name='web/index.html'), name='contact'),
-    path('track/', TemplateView.as_view(template_name='web/index.html'), name='track'),
 
 ]
--- web/url.py
+++ web/url.py
@@ -3,10 +3,10 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='web/index.html'), name='home'),
-    path('about/', TemplateView.as_view(template_name='web/index.html'), name='about'),
-    path('our-services/', TemplateView.as_view(template_name='web/index.html'), name='services'),
-    path('contact/', TemplateView.as_view(template_name='web/index.html'), name='contact'),
+    path('about/', TemplateView.as_view(template_name='web/about.html'), name='about'),
+    path('our-services/', TemplateView.as_view(template_name='web/services.html'), name='services'),
+    path('contact/', TemplateView.as_view(template_name='web/contact.html'), name='contact'),
     path('track/', TemplateView.as_view(template_name='web/index.html'), name='track'),
-    path('news/', TemplateView.as_view(template_name='web/index.html'), name='news'),
+    path('news/', TemplateView.as_view(template_name='web/news.html'), name='news'),
 
 ]
